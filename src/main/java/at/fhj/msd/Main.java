95410d5adba416c088ae3e8cdb62c1a6e684b1ad
Mostafa Shikh Ahmad <mostafa.shikhahmad@edu.fh-joanneum.at>
2024-05-14 22:51:03 +0200

    Add the print functionality

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -3,6 +3,10 @@ package at.fhj.msd;
 public class Main {
     public static void main(String[] args) {
         Calculator c = new Calculator();
+        String vorname = "Mostafa";
+        String nachname = "Shikh Ahmad";
+
+        System.out.println(vorname + " " + nachname);
         System.out.println(c.add(1,2));
         System.out.println(c.minus(2,1));
         System.out.println(c.divide(3,2));
