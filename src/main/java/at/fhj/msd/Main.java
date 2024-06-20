299e34c9135e27e3663486fea259928875bbe676
Mostafa Shikh Ahmad <mostafa.shikhahmad@edu.fh-joanneum.at>
2024-06-20 16:46:30 +0200

    .

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,10 +1,17 @@
 package at.fhj.msd;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         Calculator c = new Calculator();
         String vorname = "Mostafa";
         String nachname = "Shikh Ahmad";
+
+
+        logger.info("Info log message");
+        logger.error("Error log message");
 
         System.out.println(vorname + " " + nachname);
         System.out.println(c.add(1,2));
